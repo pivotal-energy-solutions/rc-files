a8c98b0fc4c0d2c2f1f9b8204235124d22b9825e
Steven Klass <rh0dium@users.noreply.github.com>
2018-08-07 15:44:27 -0700

    Quiet the noise a bit..

--- bin/get-infrastructure.sh
+++ bin/get-infrastructure.sh
@@ -3,7 +3,6 @@
 # This will pull the infrastructure module and install it
 # curl -sSL --retry 5 https://github.com/pivotal-energy-solutions/rc-files/raw/master/bin/get-infrastructure.sh | sh -s -- -c beta
 
-echo "Building the stack"
 
 if ! [ -x "$(command -v sudo)" ]; then
     echo 'Error: sudo is not installed.' >&2
@@ -12,7 +11,7 @@ fi
 
 if ! [ -x "$(command -v python3)" ]; then
     PYTHON_VERSION=3.7.0
-    echo 'Error: ${PYTHON_VERSION} is not installed.' >&2
+    echo '${PYTHON_VERSION} is not installed.' >&2
     sudo yum -y groupinstall "development tools"
     sudo yum -y install openssl-libs openssl-devel bzip2-devel zlib zlib-devel libffi-devel wget git nmap-ncat which
     # Build up Python 3.7
@@ -36,7 +35,7 @@ if ! [ -x "$(command -v python3)" ]; then
 fi
 
 if ! [ -x "$(command -v git)" ]; then
-    echo "Installing git"
+    echo "Git is not Installing"
     sudo yum install -y git
 fi
 
@@ -47,6 +46,6 @@ sudo pip3 install -qq --upgrade pipenv
 # "Ensuring that we can connect to github over ssh"
 ssh-keygen -F github.com >/dev/null || sudo ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
 
-pip3 install -qq --upgrade --no-cache-dir --user git+ssh://git@github.com/pivotal-energy-solutions/tensor-infrastructure.git
+pip3 install -q --upgrade --no-cache-dir --user git+ssh://git@github.com/pivotal-energy-solutions/tensor-infrastructure.git
 
 create_or_update_ami.py $@
