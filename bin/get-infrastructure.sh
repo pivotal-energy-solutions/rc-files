43d59fdb51a9b084a7074ac5dad89ca5e7a4309e
Steven Klass <rh0dium@users.noreply.github.com>
2018-08-07 11:23:12 -0700

    Added python and pip install process.

--- bin/get-infrastructure.sh
+++ bin/get-infrastructure.sh
@@ -6,14 +6,20 @@
 echo "Pulling data"
 
 if ! [ -x "$(command -v sudo)" ]; then
-  echo 'Error: sudo is not installed.' >&2
-  exit 1
+    echo 'Error: sudo is not installed.' >&2
+    exit 1
 fi
 
-if ! [ -x "$(command -v pip)" ]; then
-  echo 'Error: pip is not installed.' >&2
-  exit 1
+if ! [ -x "$(command -v python2.7)" ]; then
+    echo 'Error: sudo is not installed.' >&2
+    exit 1
 fi
+
+
+if ! [ -x "$(command -v pip)" ]; then
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
+fi
+
 
 #sudo apt-get install python-pip python-dev build-essential
 #
