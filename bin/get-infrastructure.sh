fb8c79057e9baa09bf4079103fa33d3ca7e8a474
Steven Klass <rh0dium@users.noreply.github.com>
2018-08-07 11:11:44 -0700

    Install baseline.  Lots of moving parts.

---
+++ bin/get-infrastructure.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This will pull the infrastructure module and install it
+# curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-p.py |
+
+echo "Pulling data"
+
+sudo pip install
