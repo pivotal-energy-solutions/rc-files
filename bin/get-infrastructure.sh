c84044f76bbb3762bcf5c44c3010e6ab96e7742f
Steven Klass <rh0dium@users.noreply.github.com>
2018-08-07 11:19:15 -0700

    Updated to detect missing pip as we need this.

--- bin/get-infrastructure.sh
+++ bin/get-infrastructure.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
 # This will pull the infrastructure module and install it
-# curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-p.py |
+# curl --silent --show-error --retry 5 https://raw.githubusercontent.com/pivotal-energy-solutions/rc-files/master/bin/get-infrastructure.sh | sh
 
 echo "Pulling data"
 
-sudo pip install
+if ! [ -x "$(command -v sudo)" ]; then
+  echo 'Error: sudo is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v pip)" ]; then
+  echo 'Error: pip is not installed.' >&2
+  exit 1
+fi
+
+#sudo apt-get install python-pip python-dev build-essential
+#
+#sudo pip install
