fb1b4b061009ca0cb4b07526539941518d61be97
Steven Klass <rh0dium@users.noreply.github.com>
2018-08-07 11:33:35 -0700

    Git needs to be installed too.

--- bin/get-infrastructure.sh
+++ bin/get-infrastructure.sh
@@ -3,7 +3,7 @@
 # This will pull the infrastructure module and install it
 # curl --silent --show-error --retry 5 https://raw.githubusercontent.com/pivotal-energy-solutions/rc-files/master/bin/get-infrastructure.sh | sh
 
-echo "Pulling data"
+echo "Building the stack"
 
 if ! [ -x "$(command -v sudo)" ]; then
     echo 'Error: sudo is not installed.' >&2
@@ -11,16 +11,19 @@ if ! [ -x "$(command -v sudo)" ]; then
 fi
 
 if ! [ -x "$(command -v python2.7)" ]; then
-    echo 'Error: sudo is not installed.' >&2
+    echo 'Error: python2.7 is not installed.' >&2
     exit 1
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+    echo "Installing git"
+    yum install -y git
 fi
 
 
 if ! [ -x "$(command -v pip)" ]; then
+    echo "Installing python pip"
     curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
 fi
 
-
-#sudo apt-get install python-pip python-dev build-essential
-#
-#sudo pip install
+sudo pip install https://github.com/pivotal-energy-solutions/tensor-infrastructure.git
